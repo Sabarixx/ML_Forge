7ad58b2ea330bd90481fcb33e39eb6cd6087c245
Sabarixx <saba47559@gmail.com>
2026-06-21 10:43:13 +0530

    Point frontend to live backend URL

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://ml-forge.onrender.com";
 
 async function apiRequest(endpoint, method = "GET", body = null, useAuth = false) {
     const headers = { "Content-Type": "application/json" };
